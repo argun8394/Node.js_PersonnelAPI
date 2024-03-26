39e962056c91c309e4569eb68de8173a3b243f9e
argun8394 <argundoganali@gmail.com>
2024-03-26 17:38:58 +0300

    deparment_router

--- src/routes/department.router.js
+++ src/routes/department.router.js
@@ -17,7 +17,8 @@ router.route('/:id')
     .patch(department.update)
     .delete(department.delete)
 
-// router.get('/:id/personnels', department.personnels)
+//Calling personnels from a specific department
+router.get('/:id/personnels', department.personnels) //http://127.0.0.1:8000/departments/6601812d8ae721b1a4efffb2/personnels
 
 /* ------------------------------------------------------- */
 module.exports = router
