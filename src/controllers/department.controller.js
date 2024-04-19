8f7259f9914a820540923bc7729e8f85cbd5573c
argun8394 <argundoganali@gmail.com>
2024-04-19 12:54:16 +0300

    up

--- src/controllers/department.controller.js
+++ src/controllers/department.controller.js
@@ -11,6 +11,7 @@ module.exports = {
         const data = await res.getModelList(Department)
 
         res.status(200).send({
+            isLogin: req.isLogin,
             error: false,
             detail: await res.getModelListDetails(Department),
             data // data: data
