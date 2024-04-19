e62cfde53420a1cafe02cf6783ea102d1c8addc1
argun8394 <argundoganali@gmail.com>
2024-04-19 12:54:34 +0300

    login_middleware

--- index.js
+++ index.js
@@ -33,6 +33,26 @@ app.use(require('cookie-session')({ secret: process.env.SECRET_KEY }))
 //res.getModelList()
 app.use(require('./src/middlewares/findSearchSortPage'))
 
+// Login/Logout Control Middleware 
+app.use(async (req, res, next) => {
+
+    const Personnel = require('./src/models/personnel.model')
+
+    req.isLogin = false
+
+    if (req.session?.id) {
+
+        const user = await Personnel.findOne({ _id: req.session.id })
+
+        // if (user && user.password == req.session.password) { 
+        //     req.isLogin = true
+        // }
+        req.isLogin = user && user.password == req.session.password
+    }
+    console.log('isLogin: ', req.isLogin)
+
+    next()
+})
 
 /*----------------*/
 //Routes
@@ -41,7 +61,9 @@ app.use(require('./src/middlewares/findSearchSortPage'))
 app.all('/', (req, res) => {
     res.send({
         error: false,
-        message: 'Wellcome to PERSONNEL API'
+        message: 'Welcome to PERSONNEL API',
+        session: req.session,
+        isLogin: req.isLogin
     })
 })
 
